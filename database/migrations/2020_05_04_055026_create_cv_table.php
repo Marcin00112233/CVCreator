c40e75e3ff2640f9a8a6e6cb7b20c03f8113afea
Marcin Bartliński <marcin.bartlinski@gmailcom>
2020-05-08 03:55:03 +0200

    I added cvname table

--- database/migrations/2020_05_04_055026_create_cv_table.php
+++ database/migrations/2020_05_04_055026_create_cv_table.php
@@ -16,13 +16,13 @@ class CreateCvTable extends Migration
         Schema::create('cv', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $tables = ['users','experience','education','projects','skills','extraskills','tools','extratools','courses','extrainformations','consent','aboutme','job','personaldata'];
+            $tables = ['cv_id','users','experience','education','projects','skills','extraskills','tools','extratools','courses','extrainformations','consent','aboutme','job','personaldata'];
             foreach($tables as $value){
                 if($value == 'extraskills' || $value == 'extratools') $table->boolean($value);
                 $table->unsignedBigInteger($value . '_id');
             }
             foreach($tables as $value){
-                if($value != 'extraskills' && $value != 'extratools') $table->foreign($value. '_id')->references('id')->on($value);
+                if($value != 'extraskills' && $value != 'extratools' && $value != 'cv_id') $table->foreign($value. '_id')->references('id')->on($value);
             }
 
         });
