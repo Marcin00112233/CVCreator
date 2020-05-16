56857e15314a8dea49daf0ba4d33a1b4d5ebe2ed
Marcin Bartliński <marcin.bartlinski@gmailcom>
2020-05-17 01:55:41 +0200

    I updated migration

---
+++ database/migrations/experiencepackage_2020-05-16-20-41-16.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 16 Maj 2020, 20:41
+-- Wersja serwera: 10.4.11-MariaDB
+-- Wersja PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `cvcreator`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `educationpackage`
+--
+
+CREATE TABLE `experiencepackage` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_package` bigint(20) UNSIGNED NOT NULL,
+  `sequence` tinyint(3) UNSIGNED NOT NULL,
+  `id_experience` bigint(20) UNSIGNED NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `aboutmepackage`
+--
+ALTER TABLE `experiencepackage`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `aboutmepackage`
+--
+ALTER TABLE `experiencepackage`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
